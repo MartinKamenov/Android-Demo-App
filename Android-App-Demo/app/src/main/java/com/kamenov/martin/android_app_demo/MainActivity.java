6728d98e5b8233324340e3ab20b66797b2fcfa69
MartinKamenov <martinkamenov10@gmail.com>
2018-11-11 10:31:34 +0200

    Added demo for adding elements in a LinearLayout with scroll view

--- Android-App-Demo/app/src/main/java/com/kamenov/martin/android_app_demo/MainActivity.java
+++ Android-App-Demo/app/src/main/java/com/kamenov/martin/android_app_demo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.kamenov.martin.android_app_demo;
 
+import android.graphics.Color;
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mAddViewButton;
+    private LinearLayout mContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mContainer = (LinearLayout)findViewById(R.id.container);
+        mAddViewButton = (Button)findViewById(R.id.add_view_btn);
+        mAddViewButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+        TextView textView = new TextView(this);
+        textView.setText("New view");
+        textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 35);
+        mContainer.addView(textView);
     }
 }
